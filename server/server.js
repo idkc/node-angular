f1d2627888f40d8e2a930f5ed970abeaa0208d91
Ian Shoemaker <iss90g@gmail.com>
2013-09-23 13:56:54 -0400

    initial commit

---
+++ server/server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = module.exports = express();
+
+var splitList = __dirname.split('/');
+var clientDir = splitList.splice(0, splitList.length -1).join('/') + '/client';
+
+app.configure(function(){
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(express.cookieParser('idkc'));
+	app.use(express.session());
+	app.use(app.router);
+});
+console.log(__dirname);
+
+app.get(/\/bower_components|resources|views|modules\/?.*/, function(req, res){
+	res.sendfile(clientDir + req.path);
+});
+
+app.get('/*', function(req, res){
+	res.sendfile(clientDir + '/index.html');
+});
+
+app.configure('development', function(){
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+	app.use(express.errorHandler());
+});
+
+app.listen(8080, function(){
+	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
